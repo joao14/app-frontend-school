97fea199a3b12d3c2c3067197d7c3fb36682f230
alex-merino <alexander.merino@pedidosya.com>
2021-11-27 02:16:27 -0500

    Added new changues

--- src/app/pages/layout/crm/users/edit/edit.component.ts
+++ src/app/pages/layout/crm/users/edit/edit.component.ts
@@ -3,17 +3,24 @@ import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { MessageService, SelectItem } from 'primeng';
 import { obra } from 'src/models/obra';
+import { obras } from 'src/models/obras';
 import { ApisService } from 'src/services/apis.service';
 import { UtilService } from 'src/services/util.service';
 
-export interface users {
+export interface user {
   usuaApellidos: string;
   usuaClave: string;
   usuaEstado: boolean;
   usuaId: number;
   usuaNick: string;
   usuaNombres: string;
-  usuaPerfil: string;
+  usuaPerfil: any;
+}
+
+
+export interface users {
+  user: user;
+  obras: Array<obra>;
 }
 
 
@@ -53,23 +60,56 @@ export class EditComponent implements OnInit {
   }
 
   inicializateValores() {
+
+    this.user = null;
     this.perfiles = [
       { name: 'Administrador', code: 'Administrador' },
       { name: 'Gestor', code: 'Gestor' }
     ];
+
     this.user = {
-      usuaApellidos: this.userTemp != null ? this.userTemp['usuaApellidos'] : null,
-      usuaClave: this.userTemp != null ? this.userTemp['usuaClave'] : null,
-      usuaEstado: this.userTemp != null ? this.userTemp['usuaEstado'] : null,
-      usuaId: this.userTemp != null ? this.userTemp['usuaId'] : null,
-      usuaNick: this.userTemp != null ? this.userTemp['usuaNick'] : null,
-      usuaNombres: this.userTemp != null ? this.userTemp['usuaNombres'] : null,
-      usuaPerfil: this.userTemp != null ? this.userTemp['usuaPerfil'] : null
+      user: {
+        usuaApellidos: this.userTemp != undefined ? this.userTemp['user'].usuaApellidos : null,
+        usuaClave: this.userTemp != undefined ? this.userTemp['user'].usuaClave : null,
+        usuaEstado: this.userTemp != undefined ? this.userTemp['user'].usuaEstado : null,
+        usuaId: this.userTemp != undefined ? this.userTemp['user'].usuaId : null,
+        usuaNick: this.userTemp != undefined ? this.userTemp['user'].usuaNick : null,
+        usuaNombres: this.userTemp != undefined ? this.userTemp['user'].usuaNombres : null,
+        usuaPerfil: this.userTemp != undefined ? this.getSelectPerfiles(this.userTemp['user'].usuaPerfil)[0] : null
+      },
+      obras: this.userTemp != undefined ? this.getObrasListas(this.userTemp['obras']) : [],
     }
 
-    console.log("Este es el usurio seleccionado");
-    console.log(this.user);
+    this.obrasSelect = this.userTemp != undefined ? this.user.obras : [];
 
+    console.log("USER?????..");
+    console.log(this.user);
+    console.log("OBRAS INICIALLLLLL?????");
+    console.log(this.obrasSelect);
+
+
+
+  }
+
+  getObrasListas(obras: any[]) {
+    let obra_: Array<obra> = [];
+    obras.forEach(obra => {
+      obra_.push({
+        obraDescripcion: obra.obraDescripcion,
+        obraDireccion: obra.obraDireccion,
+        obraId: obra.obraId,
+        obraLogo1: obra.obraLogo1,
+        obraLogo2: obra.obraLogo2,
+        obraNombre: obra.obraNombre
+      })
+    })
+
+    return obra_
+  }
+
+
+  getSelectPerfiles(name: string) {
+    return this.perfiles.filter(element => element.name == name)
   }
 
   save() {
@@ -88,17 +128,17 @@ export class EditComponent implements OnInit {
   }
 
   getObras() {
-    console.log("Consultar obras...");
-
+    console.log("OBRAS CONSULTADAS");
     this.utilService.isLoading.next(true);
     this.api.getobras(localStorage.getItem("token")).then(data => {
-
-      console.log(data);
 
       if (data.headerApp.code == 200 || data.headerApp.code == 0) {
         this.obras = data.data.obras
         this.utilService.isLoading.next(false);
       }
+      console.log("DTATA");
+
+      console.log(data.data.obras);
     }).catch(err => {
       this.utilService.isLoading.next(false);
       if (err.error.code == 401) {
--- src/app/pages/layout/crm/users/users.component.ts
+++ src/app/pages/layout/crm/users/users.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { MessageService } from 'primeng';
+import { obra } from 'src/models/obra';
 import { ApisService } from 'src/services/apis.service';
 import { UtilService } from 'src/services/util.service';
 
-export interface users {
+export interface user {
   usuaApellidos: string;
   usuaClave: string;
   usuaEstado: boolean;
@@ -15,6 +16,13 @@ export interface users {
   usuaPerfil: string;
 }
 
+
+export interface users {
+  user: user;
+  obras: Array<obra>;
+}
+
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -23,7 +31,9 @@ export interface users {
 })
 export class UsersComponent implements OnInit {
 
-  usuarios: Array<users> = []
+  user: user
+  obras: Array<obra> = []
+  users: Array<users> = []
 
   constructor(private apis: ApisService, private router: Router, private utilservice: UtilService) {
   }
@@ -35,20 +45,26 @@ export class UsersComponent implements OnInit {
   async getUsuarios() {
     this.utilservice.isLoading.next(true);
     await this.apis.getusuarios(localStorage.getItem("token")).then(async data => {
-      console.log("Usuarios");
-      console.log(data);
       if (data.headerApp.code == 200) {
         data.data.forEach(element => {
-          this.usuarios.push({
-            usuaApellidos: element["usuaApellidos"],
-            usuaClave: element["usuaClave"],
-            usuaEstado: element["usuaEstado"],
-            usuaId: element["usuaId"],
-            usuaNick: element["usuaNick"],
-            usuaNombres: element["usuaNombres"],
-            usuaPerfil: element["usuaPerfil"]
+          this.user = {
+            usuaApellidos: element.user["usuaApellidos"],
+            usuaClave: element.user["usuaClave"],
+            usuaEstado: element.user["usuaEstado"],
+            usuaId: element.user["usuaId"],
+            usuaNick: element.user["usuaNick"],
+            usuaNombres: element.user["usuaNombres"],
+            usuaPerfil: element.user["usuaPerfil"]
+          }
+          this.obras = element.obras
+
+          this.users.push({
+            user: this.user,
+            obras: this.obras
           })
+
         });
+
         this.utilservice.isLoading.next(false);
       }
 
